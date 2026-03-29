f93f09eb03dd25a12c05b9dda1beb7b166110b44
Bill <89919489+Billcyferka@users.noreply.github.com>
2026-03-29 20:13:42 +0200

    more sed

--- tools/tgs_scripts/PreCompile.sh
+++ tools/tgs_scripts/PreCompile.sh
@@ -35,7 +35,7 @@ cargo build --release --target="$RUST_TARGET" --features allow_non_32bit
 cp -f "target/$RUST_TARGET/release/librust_g.so" "$1/librust_g.so"
 cd ..
 
-# 3. Build dreamluau with ARM64 Source Patches
+# 3. Build dreamluau with ARM64 patches
 if [ ! -d "dreamluau" ]; then
     git clone https://github.com/tgstation/dreamluau
 fi
@@ -53,13 +53,11 @@ if [ -d "$MEOW_DIR" ]; then
     sed -i 's/Some(result)/Some(result.try_into().unwrap())/g' "$MEOW_DIR/crates/core/src/value/reference.rs"
 fi
 
-echo "Patching DreamLuau source for ARM64 signed/unsigned char fix..."
-# Fix the "expected *const u8, found *const i8" errors
+echo "Patching DreamLuau source for ARM64 pointer types..."
+# Fixes the 8 'expected *const u8, found *const i8' and 'i8 to u8' errors
 sed -i 's/as \*const i8/as \*const u8/g' src/state/util/entrypoint.rs
 sed -i 's/as \*const i8/as \*const u8/g' src/state/util/traceback.rs
-
-# Fix the "expected u8, found i8" errors in traceback.rs
-sed -i "s/as i8)/as u8)/g" src/state/util/traceback.rs
+sed -i 's/as i8/as u8/g' src/state/util/traceback.rs
 
 # Now build
 LIBCLANG_PATH=/usr/lib/aarch64-linux-gnu/ cargo build --release --target="$RUST_TARGET"
